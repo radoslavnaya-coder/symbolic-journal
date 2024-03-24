d4668922a2ef927f530f1b3cfa17b787d8d342bf
radoslavnaya-coder <killaarte13@gmail.com>
2024-03-24 22:41:19 +0400

    add component banner

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # symbol
 
-This template should help get you started developing with Vue 3 in Vite.
+This is my project about journal with my university
 
 ## Recommended IDE Setup
 
---
+++ src/views/Header/TheBanner.vue
@@ -0,0 +1,36 @@
+<template>
+    <img class="banner" src="@/assets/img/banner.jpg" alt="banner">
+    <div class="menu">
+        <a>
+            <img src="/menu.svg" alt="menu">
+        </a>
+    </div>
+    <div class="menu">
+        <a href=""><img src="/menu.svg" alt="menu"></a>
+        <a href=""><img src="/create.svg" alt="create"></a>
+        <a href=""><img src="/alert.svg" alt="alert"></a>
+        <a href=""><img src="/favourite.svg" alt="favourite"></a>
+        <a href=""><img src="/user.svg" alt="user"></a>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TheBanner'
+}
+</script>
+<style scoped>
+.banner{
+    position: absolute;
+}
+.menu{
+    display: block;
+    height: 20rem;
+    position: relative;
+    text-align: right;
+    background: url(@/assets/img/menu-bg-small.png) no-repeat top right;
+    padding: 5rem 2rem;
+}
+.menu img{
+    max-width: 70px;
+}
+</style>
